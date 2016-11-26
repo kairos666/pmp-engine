1016d941d5726cb7f2afb900e1ee91bf37dc59a3
David Maggi <david.maggi@orange.com>
2016-11-26 10:29:27 +0100

    #17 refactored plugin finder

--- engine/plugins-finder.js
+++ engine/plugins-finder.js
@@ -1,29 +1,72 @@
 'use strict'
 
-const exec              = require('child_process').exec;
-const Q                 = require('q');
+const exec                   = require('child_process').exec;
+const Q                      = require('q');
+const readFile               = require('fs').readFile;
+const path                   = require('path');
+const packageEntryPoint      = 'index.js';
+const packageDescriptorFile  = 'package.json';
+const packageReadmeFile      = 'README.md';
+
+// description PROMISER
+let descPromiser = packageName => {
+  let descPromise            = Q.defer();
+  let packagePath;
+
+  // try requiring the plugin
+  try {
+    packagePath = require.resolve(packageName);
+  } catch(e) {
+    descPromise.reject(`can't require ${packageName}`);
+    return descPromise.promise;
+  }
+
+  let packageJsonPath        = path.resolve(packagePath.replace(packageEntryPoint, ''), '..', packageDescriptorFile);
+
+  readFile(packageJsonPath, 'utf8', (err, data) => {
+    if(err) {
+      // in case of a fail - resolve anyway
+      descPromise.resolve('');
+    } else {
+      let descObj = JSON.parse(data);
+      descPromise.resolve(descObj.description);
+    }
+  });
+
+  return descPromise.promise;
+}
+
+// readme PROMISER
+let readmePromiser = packageName => {
+  let readmePromise          = Q.defer();
+  let packagePath;
+
+  // try requiring the plugin
+  try {
+    packagePath = require.resolve(packageName);
+  } catch(e) {
+    readmePromise.reject(`can't require ${packageName}`);
+    return descPromise.promise;
+  }
+  let packageReadmePath      = path.resolve(packagePath.replace(packageEntryPoint, ''), '..', packageReadmeFile);
+
+  readFile(packageReadmePath, 'utf8', (err, data) => {
+    if(err) {
+      // in case of a fail - resolve anyway
+      readmePromise.resolve('');
+    } else {
+      readmePromise.resolve(data);
+    }
+  });
+
+  return readmePromise.promise;
+}
 
 class PmpPluginDescriptor {
-  constructor(packageName) {
-    const readFileSync           = require('fs').readFileSync;
-    const path                   = require('path');
-    const packageEntryPoint      = 'index.js';
-    const packageDescriptorFile  = 'package.json';
-    const packageReadmeFile      = 'README.md';
-
+  constructor(packageName, packageDescription, packageReadme) {
     this.packageName             = packageName;
-
-    // get package files path
-    let packagePath              = require.resolve(this.packageName);
-    let packageJsonPath          = path.resolve(packagePath.replace(packageEntryPoint, ''), '..', packageDescriptorFile);
-    let packageReadmePath        = path.resolve(packagePath.replace(packageEntryPoint, ''), '..', packageReadmeFile);
-
-    // get package description
-    let packageJsonObject        = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
-    this.packageDescription      = packageJsonObject.description;
-
-    // get package readme (markdown)
-    this.packageReadme           = readFileSync(packageReadmePath, 'utf8');
+    this.packageDescription      = packageDescription;
+    this.packageReadme           = packageReadme;
   }
 }
 
@@ -32,25 +75,44 @@ class PmpPluginDescriptor {
 =========================================================================== */
 let getAvailablePmpPluginsPromise = (pluginNamePattern) => {
   let deferredPluginList = Q.defer();
+
+  // CHAIN 1 - filter packages
+  let filteredPluginsPromise = deferredPluginList.promise.then(packageJson => {
+    // list all packages
+    let packageArray = Object.keys(packageJson.dependencies);
+
+    // filter out pmp plugins
+    let filteredPackageArray = packageArray.filter(pack => (pack.indexOf(pluginNamePattern) === 0));
+
+    return filteredPackageArray;
+  });
+
+  // CHAIN 2 - resolve description/readme
+  let resolvedPluginsPromisesArray = filteredPluginsPromise.then(filteredPackageArray => {
+    let PluginPromisesArray = [];
+
+    filteredPackageArray.forEach(packageName => {
+      let descPromise           = descPromiser(packageName);
+      let readmePromise         = readmePromiser(packageName);
+
+      PluginPromisesArray.push(Q.all([
+          descPromise,
+          readmePromise
+        ]).spread((desc, readme) => {
+          return new PmpPluginDescriptor(packageName, desc, readme);
+      }));
+    });
+
+    return PluginPromisesArray;
+  });
+
+  // execute promise start
   exec('npm ls --json', function(err, stdout, stderr) {
     deferredPluginList.resolve(JSON.parse(stdout));
   });
 
-  return deferredPluginList.promise.then(packageJson => {
-    // list all packages
-    let packageArray = Object.keys(packageJson.dependencies);
-
-    //filter out pmp plugins
-    let filteredPackageArray = packageArray.filter(pack => (pack.indexOf(pluginNamePattern) === 0));
-
-    // create pmpPluginDescriptors
-    let result = [];
-
-    filteredPackageArray.forEach(packageName => {
-      result.push(new PmpPluginDescriptor(packageName));
-    });
-
-    return result;
+  return Q.allSettled(resolvedPluginsPromisesArray).then(results => {
+    return results.filter(result => (result.state !== 'rejected')).map(result => result.value);
   });
 };
 
